19725d960a0c8e0c6cf77e8fa737bd3b36000558
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-01-05 18:51:38 -0500

    💄 Add white background to styled boxes

--- src/components/Styled/StyledBox.jsx
+++ src/components/Styled/StyledBox.jsx
@@ -3,8 +3,9 @@ import { Box } from "@rebass/grid";
 
 const StyledBox = styled(Box)`
   border-radius: 2rem;
-  box-shadow: 0 10px 10px rgba(0,0,0,0.01), 0 3px 3px rgba(0,0,0,0.10);
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.01), 0 3px 3px rgba(0, 0, 0, 0.1);
   padding: 1rem;
-`
+  background: ${props => props.theme.colors.white};
+`;
 
 export default StyledBox;
