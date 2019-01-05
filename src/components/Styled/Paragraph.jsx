a817650b8f238d1d32cb13d1db04c0bbdf35fced
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-01-05 16:37:51 -0500

    💄 Add prop to change text size in paragraphs

--- src/components/Styled/Paragraph.jsx
+++ src/components/Styled/Paragraph.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const Paragraph = styled.p`
   line-height: 1.5;
   letter-spacing: 0.5px;
+  ${props => props.small && "font-size: 1.4rem" }
 `;
 
 export default Paragraph;
