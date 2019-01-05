bd99102dd0492a6f3eadfe3862462b9cb2f490eb
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-01-04 19:49:45 -0500

    💄 Improve styling of header

--- src/components/Styled/H1.jsx
+++ src/components/Styled/H1.jsx
@@ -3,5 +3,5 @@ import styled from "styled-components";
 const H1 = styled.h1`
   font-size: 3.6rem;
   color: ${props => props.theme.colors.navy};
-`
+`;
 export default H1;
--- src/components/Styled/H2.jsx
+++ src/components/Styled/H2.jsx
@@ -1,7 +1,21 @@
 import styled from "styled-components";
 
 const H2 = styled.h2`
-  font-size: 3rem;
+  font-size: 4.8rem;
+  text-align: center;
+  text-transform: uppercase;
   color: ${props => props.theme.colors.primary};
+  position: relative;
+  &:after {
+    position: absolute;
+    content: "";
+    width: 16rem;
+    height: 1.6rem;
+    top: 4rem;
+    left: 50%;
+    margin-left: -8rem;
+    background: ${props => props.theme.colors.green};
+    z-index: -1;
+  }
 `;
 export default H2;
