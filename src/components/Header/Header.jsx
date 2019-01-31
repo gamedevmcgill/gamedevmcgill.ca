221beeeaa9ed38aef4e5d80d4b4d9a5accddccf8
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-01-30 19:32:55 -0500

    💄 Make header text more readable

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,16 +12,16 @@ const Header = ({ title, img, author, tags, date }) => (
     <ReturnLink />
     <Box mb="20rem">
       <MaxWidthBox maxWidth="80rem" mb="2rem">
-        <H1 light>{title}</H1>
+        <H1 style={{ marginBottom: "0.4rem" }}>{title}</H1>
+        <Flex
+          justifyContent="space-around"
+          style={{ fontSize: "1.4rem", fontWeight: "700" }}
+        >
+          {author && <p>by {author}</p>}
+          {date && <p>{date}</p>}
+        </Flex>
       </MaxWidthBox>
       {tags && <PostTags tags={tags} />}
-      <Flex
-        justifyContent="space-around"
-        style={{ color: "white", fontSize: "1.4rem", fontWeight: "700" }}
-      >
-        {author && <p>by {author}</p>}
-        {date && <p>{date}</p>}
-      </Flex>
     </Box>
   </Container>
 );
--- src/components/Styled/H1.jsx
+++ src/components/Styled/H1.jsx
@@ -4,6 +4,6 @@ const H1 = styled.h1`
   font-size: 4.8rem;
   font-weight: 900;
   color: ${props =>
-    props.light ? props.theme.colors.white : props.theme.colors.navy};
+    props.light ? props.theme.colors.white : props.theme.colors.black};
 `;
 export default H1;
