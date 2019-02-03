715d4ef935dad3aadf89800d1dba91c0e7a85f00
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-02-02 23:29:01 -0500

    💄 Update sizing of Events

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -7,22 +7,28 @@ import Paragraph from "../Styled/Paragraph";
 const Events = ({ events }) => (
   <Flex flexWrap="wrap" alignItems="space-between">
     {events.map(e => (
-      <StyledBox width={[1, "45%", "22.5%"]} key={e.title} m="0 1rem" p="1rem">
+      <StyledBox width={[1, "45%", "22.5%"]} key={e.title} m="1rem" p="1rem">
         <article>
-          <Title>{e.title}</Title>
-          <img
-            style={{
-              objectFit: "contain",
-              width: "100%",
-              height: "100%"
-            }}
-            src={e.image}
-            alt={e.title}
-          />
-          <Paragraph small>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
-          </Paragraph>
+          <Flex flexDirection="column" alignItems="center">
+            <Title>{e.title}</Title>
+            <img
+              style={{
+                objectFit: "contain",
+                width: "50%",
+                height: "100%"
+              }}
+              src={e.image}
+              alt={e.title}
+            />
+            <Paragraph small>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </Paragraph>
+          </Flex>
         </article>
       </StyledBox>
     ))}
