eb4a79ffddd749ff910cc8d2302a78992a045cdf
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-03-04 01:06:07 -0500

    💄Add box to team component

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -4,29 +4,31 @@ import Name from "./Name";
 import Portrait from "./Portrait";
 import Position from "./Position";
 import Game from "./Game";
+import StyledBox from "../Styled/StyledBox";
 
 const Team = ({ members }) => (
-  <Flex flexWrap="wrap">
-    {members &&
-      members.map(m => (
-        <Box m="2rem 0" width={[1, "50%", "33%"]} key={m.name}>
-          {console.log(m.name, m.portrait)}
-          <article>
-            <Flex flexDirection="column" alignItems="center">
-              <Portrait src={m.portrait.publicURL} />
-              <Name>{m.name}</Name>
-              <Position>POSITION: {m.position}</Position>
-              <Game>
-                <span role="img" aria-label="Favorite Game">
-                  ❤️ 🕹
-                </span>{" "}
-                : {m.game}
-              </Game>
-            </Flex>
-          </article>
-        </Box>
-      ))}
-  </Flex>
+  <StyledBox p="4rem">
+    <Flex flexWrap="wrap">
+      {members &&
+        members.map(m => (
+          <Box m="2rem 0" width={[1, "50%", "33%"]} key={m.name}>
+            <article>
+              <Flex flexDirection="column" alignItems="center">
+                <Portrait src={m.portrait.publicURL} />
+                <Name>{m.name}</Name>
+                <Position>POSITION: {m.position}</Position>
+                <Game>
+                  <span role="img" aria-label="Favorite Game">
+                    ❤️ 🕹
+                  </span>{" "}
+                  : {m.game}
+                </Game>
+              </Flex>
+            </article>
+          </Box>
+        ))}
+    </Flex>
+  </StyledBox>
 );
 
 export default Team;
