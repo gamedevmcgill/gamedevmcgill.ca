aaa0054b6744240fc89a1234bf639aa7c73373ef
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-03-04 13:50:03 -0500

    🐛 Add path prefix

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -4,7 +4,7 @@ module.exports = {
   siteTitleAlt: "Game Dev McGill", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.jpg", // Logo used for SEO and manifest.
   siteUrl: "https://vagr9k.github.io", // Domain of your website without pathPrefix.
-  pathPrefix: "", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "Game Development Student Society @ McGill University.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
