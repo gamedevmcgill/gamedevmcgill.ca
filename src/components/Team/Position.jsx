ea0a0b7a2d99ee1968ab63bed05bc19e3ab91adc
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-03-13 12:56:22 -0400

    💄 Make copy cleaner for Teams

--- src/components/Team/Position.jsx
+++ src/components/Team/Position.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Position = styled.h2`
-  color: ${props => props.theme.colors.primary};
+  color: ${props => props.theme.colors.black};
   font-weight: 800;
   font-size: 1.6rem;
   margin: 0;
