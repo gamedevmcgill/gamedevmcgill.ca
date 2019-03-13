93040e789e9f9c9feacf4e91d52d1f77c018562b
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-03-13 01:15:50 -0400

    ➖ Remove slick-carousel dependenct

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -73,6 +73,17 @@ class SEO extends Component {
         <meta name="description" content={description} />
         <meta name="image" content={image} />
         <link
+          rel="stylesheet"
+          type="text/css"
+          charset="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
+        <link
           rel="apple-touch-icon"
           sizes="57x57"
           href="/favicons/apple-icon-57x57.png"
