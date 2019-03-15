0b99345fd977dc6a15864bb26f247dd879cf17e0
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-03-14 21:56:11 -0400

    ⬆️ Add react-🔥-DOM patch

--- gatsby-node.js
+++ gatsby-node.js
@@ -176,3 +176,13 @@ exports.createPages = ({ graphql, actions }) => {
     );
   });
 };
+
+exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
+  const config = getConfig();
+  if (stage.startsWith("develop") && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "react-dom": "@hot-loader/react-dom"
+    };
+  }
+};
