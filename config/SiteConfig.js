c15319b2fc1c1ddb67fb3771b69f01154f9f4e79
Loreina Chew <loreina.chew@gmail.com>
2020-05-05 14:23:18 -0400

    fix: update copyright date

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -5,7 +5,8 @@ module.exports = {
   siteLogo: "/logos/logo-1024.jpg", // Logo used for SEO and manifest.
   siteUrl: "https://www.gamedevmcgill.ca", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "The Game Development Student Society of McGill University is a student-run club that empowers aspiring game developers (programmers, designers, artists, etc.) to hone their craft. Come join our club to collaborate, learn, and play with our members! Find out more at our info sessions at the beginning of each semester.", // Website description used for RSS feeds/meta description tag.
+  siteDescription:
+    "The Game Development Student Society of McGill University is a student-run club that empowers aspiring game developers (programmers, designers, artists, etc.) to hone their craft. Come join our club to collaborate, learn, and play with our members! Find out more at our info sessions at the beginning of each semester.", // Website description used for RSS feeds/meta description tag.
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
   googleAnalyticsID: "UA-64089703-2", // GA tracking ID.
   disqusShortname: "gdm", // Disqus shortname.
@@ -28,9 +29,9 @@ module.exports = {
     {
       label: "email",
       url: "mailto:gamedev@mcgilleus.ca",
-    }
+    },
   ],
-  copyright: "Copyright © 2019. GameDev McGill.", // Copyright string for the footer of the website and RSS feed.
+  copyright: "Copyright © 2020. GameDev McGill.", // Copyright string for the footer of the website and RSS feed.
   themeColor: "#FF0079", // Used for setting manifest and progress theme colors.
-  backgroundColor: "#e0e0e0" // Used for setting manifest background color.
+  backgroundColor: "#e0e0e0", // Used for setting manifest background color.
 };
